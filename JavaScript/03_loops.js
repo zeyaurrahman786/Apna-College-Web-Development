1100b7e77913e005be28535a24b6fa8e66ee3447
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-04-28 01:32:22 +0530

    loops

--- JavaScript/03_loops.js
+++ JavaScript/03_loops.js
@@ -1,1 +1,37 @@
 // Loops :----------------------------------------------------------------
+
+// for(let i=1; i<=5; i++){
+//     console.log(i);
+// }
+
+
+
+// for(let i=5; i>=1 ; i--){
+//     console.log(i);
+// }
+
+
+
+//  Print all odd numbers (1 to 15) :-)
+// for(let i=1; i<=15; i=i+2){
+//     console.log(i);
+// }
+
+
+
+// for(let i=15; i>=1; i=i-2){
+//         console.log(i);
+// }
+
+
+
+//  Print all even numbers (2 to 10) :-)
+// for(let i=2; i<=10; i=i+2){
+//     console.log(i);
+// }
+
+
+
+// for(let i=10; i>=2; i=i-2){
+//     console.log(i);
+// }
