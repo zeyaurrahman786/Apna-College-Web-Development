f626d141766be4fa625b3ddaad93ef413435478d
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-04-28 14:48:28 +0530

    object

---
+++ JavaScript/05_Object.js
@@ -0,0 +1,138 @@
+// Object Literals :----------------------------------------------------------------
+
+// const student = {
+//     name: "Zeyaur Rahman",
+//     age: 23,
+//     address: "Bangalore",
+//     city: "Bangalore",
+//     country: "India"
+// }
+
+
+
+
+
+
+/* Thread / Twitter Post :-) 
+Creating an object literal for the properties of thread/twitter post which includes -
+. username
+. content 
+. likes
+. reports
+. tags
+*/
+
+// const post = {
+//     username : "@zeyaur.com",
+//     content : "This is my #firstPost",
+//     likes : 150,
+//     reports : 5,
+//     tags : ["@rahama.com"]
+// };
+
+// console.log(post ["username"]);
+// console.log(post.tags);
+
+
+
+
+
+
+// const obj = {
+//     1: "a",
+//     2: "b",
+//     true: "c",
+//     null: "d",
+//     undefined: "e"
+// };
+
+
+
+
+
+
+// Add Update Values :-)
+// const student = {
+//     name: "Zeyaur Rahman",
+//     age: 23,
+//     marks: 95,
+//     address: "Bangalore",
+//     city: "Bangalore",
+//     country: "India"
+// }
+// student.city = "Bhopal";
+// student.gender = "Male";
+// student.marks = "A";
+// student.marks = [98, 92, 97,94];
+// delete student.marks;
+// console.log(student);
+
+
+
+
+
+
+// Object of Objects :-)   Storing information of multiple students.
+
+// const classInfo = {
+//     zeyaur: {
+//         grade : "A+",
+//         city : "Bhopal"
+//     },
+//     rahman: {
+//         grade : "A",
+//         city : "Delhi"
+//     },
+//     neyaj: {
+//         grade : "B",
+//         city : "Bettiah"
+//     },
+//     alam: {
+//         grade : "B+",
+//         city : "Bengaluru"
+//     }
+// };
+
+
+
+
+
+
+// Array of Objects :-)    Storing information of multiple students
+
+// const classInfo =
+// [
+//     {
+//         name : "Zeyaur",
+//         grade : "A+",
+//         city : "Bhopal"
+//     },
+
+//     {   name : "Rahman",
+//         grade : "A",
+//         city : "Delhi"},
+
+//     {
+//         name : "Neyaj",
+//         grade : "B",
+//         city : "Bettiah"
+//     },
+
+//     {
+//         name : "Alam",
+//         grade : "B+",
+//         city : "Bengaluru"
+//     }
+// ];
+
+
+
+
+
+
+// Math Object :-) 
+// Generate a random number between 1 and 100.
+
+// console.log(Math.floor(Math.random() * 100) + 1);
+
+// console.log(Math.floor(Math.random() * 5) + 1);
