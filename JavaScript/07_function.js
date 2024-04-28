e0407fb86276c94afd16ec4cd4c154676d3ffc52
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-04-29 01:03:28 +0530

    functions.

---
+++ JavaScript/07_function.js
@@ -0,0 +1,44 @@
+// Function :----------------------------------------------------------------
+
+// function hello(){
+//     console.log("Hello World!");
+// }
+// hello();
+
+
+
+
+// function printName(){
+//     console.log("Zeyaur Rahman");
+// }
+// printName();
+
+
+
+
+// function print1to5(){
+//     for(let i=1; i<=5; i++){
+//         console.log(i);
+//     }
+// }
+// print1to5();
+
+
+
+
+
+// function isAdult(){
+//     let age = 25;
+//     if(age >= 18){
+//         console.log("Adult");
+//     }
+//     else{
+//         console.log("Not Adult");
+//     }
+// }
+// isAdult();
+
+
+
+
+
