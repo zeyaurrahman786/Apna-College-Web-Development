018f5fcfaeb4a698cbaccb8f43b63393b91aa0ef
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-04-29 01:30:40 +0530

    function

--- JavaScript/07_function.js
+++ JavaScript/07_function.js
@@ -42,3 +42,63 @@
 
 
 
+// Practice Question :-) Create a function that prints a poem.
+
+// function printPoem(){
+//     console.log("Twinkle Twinkle Little Star");
+//     console.log("How I wonder what you are!");
+//     console.log("Up above the world so high,");
+//     console.log("Like a diamond in the sky.");
+// }
+// printPoem();
+
+
+
+
+
+// Practice Question :-) Create a function to roll a dice & always display the value of the dice(1 to 6).
+
+// function rollDice(){
+//     let dice = Math.floor(Math.random() * 6) + 1;
+//     console.log(dice);
+// }
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+
+
+
+
+
+// Functions with Arguments :-) Values we pass to the function.
+
+// function printName(name){
+//     console.log(name);
+// }
+// printName("Zeyaur Rahman");
+
+
+
+
+
+// function printInfo(name, age){
+//     console.log(`${name}'s age is ${age}`);
+// }
+// printInfo("Zeyaur", 23)
+
+
+
+
+
+// function sum(a, b){
+//     console.log(a+b);
+// }
+// sum(10, 25);
+
+
+
+
+
