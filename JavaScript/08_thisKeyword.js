84f636eddf4deb02094e71c7ec450270673c2601
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-04-30 00:20:41 +0530

    this keyword

---
+++ JavaScript/08_thisKeyword.js
@@ -0,0 +1,18 @@
+// This Keyword :-)
+
+// const student = {
+//     name: "Zeyaur Rahman",
+//     age: 23,
+//     English: 85,
+//     Hindi: 80,
+//     Maths: 95,
+//     Physics: 90,
+//     Chemistry: 75,
+
+//     getAvg(){
+//         console.log(this);
+//         let avg = (this.English + this.Hindi + this.Maths + this.Physics + this.Chemistry) / 5;
+//         console.log(`${this.name} got avg marks = ${avg} %`);
+//     }
+// }
+// console.log(student.getAvg());
